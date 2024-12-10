59d3b944e1045bb5b6898fb07e19891e6159ad00
mohamedsalehjr <mohamedsalehjr@gmail.com>
2024-12-10 19:08:57 +0000

    insert into bst

---
+++ Medium/InsertIntoBst.js
@@ -0,0 +1,20 @@
+// * Definition for a binary tree node.
+//  * function TreeNode(val, left, right) {
+//  *     this.val = (val===undefined ? 0 : val)
+//  *     this.left = (left===undefined ? null : left)
+//  *     this.right = (right===undefined ? null : right)
+//  * }
+
+function insertIntoBST(root, val) {
+  if (!root) {
+    return new TreeNode();
+  }
+
+  if (val < root.val) {
+    root.left = insertIntoBST(root.left, val);
+  } else if (val > root.val) {
+    root.right = insertIntoBST(root.right, val);
+  }
+
+  return root;
+}
