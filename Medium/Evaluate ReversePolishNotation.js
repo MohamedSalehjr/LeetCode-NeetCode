664466f78192730787116893741aaae11bdd6fbe
mohamedsalehjr <mohamedsalehjr@gmail.com>
2025-01-06 10:25:23 +0000

    eval reverse polish notation

---
+++ Medium/Evaluate ReversePolishNotation.js
@@ -0,0 +1,22 @@
+function evalRPN(tokens) {
+  const stack = [];
+
+  for (let c of tokens) {
+    if (c === "+") {
+      stack.push(stack.pop() + stack.pop());
+    } else if (c === "-") {
+      const a = stack.pop();
+      const b = stack.pop();
+      stack.push(b - a);
+    } else if (c === "*") {
+      stack.push(stack.pop() * stack.pop());
+    } else if (c === "/") {
+      const a = stack.pop();
+      const b = stack.pop();
+      stack.push(Math.trunc(b / a));
+    } else {
+      stack.push(parseInt(c));
+    }
+  }
+  return stack.pop();
+}
