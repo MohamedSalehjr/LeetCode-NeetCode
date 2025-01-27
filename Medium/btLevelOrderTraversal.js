39bce485b765fc3a2c40cf5e796899acdb127b90
mohamedsalehjr <mohamedsalehjr@gmail.com>
2025-01-27 07:40:03 +0000

    level order traversak

--- Medium/btLevelOrderTraversal.js
+++ Medium/btLevelOrderTraversal.js
@@ -1,32 +1,31 @@
-function levelOrder(root){
-    if(!root){
-        return []
+function levelOrder(root) {
+  if (!root) {
+    return [];
+  }
+
+  let res = [];
+  let queue = [];
+
+  queue.push(root);
+
+  while (queue.length > 0) {
+    let level = [];
+
+    for (let i = queue.length; i > 0; i--) {
+      let curr = queue.shift();
+
+      level.push(curr.val);
+
+      if (curr.left) {
+        queue.push(curr.left);
+      }
+      if (curr.right) {
+        queue.push(curr.right);
+      }
     }
-
-    let res = []
-    let queue = []
-
-    queue.push(root)
-
-    while (queue.length > 0){
-        let level = []
-
-
-        for(let i = queue.length; i > 0; i++){
-            let curr = queue.shift();
-
-            level.push(curr.val)
-            if(curr.left){
-                queue.push(curr.right)
-            }
-            if(curr.right){
-                queue.push(curr.right)
-            }
-            }
-            if(level.length > 0){
-            res.push(level)
-        }
+    if (level.length > 0) {
+      res.push(level);
     }
-
-    return res;
+  }
+  return res;
 }
