541358d85e98bea3e89993738525dcda158ff3a8
mohamedsalehjr <mohamedsalehjr@gmail.com>
2025-02-08 14:21:44 +0000

    permutation in string

--- Medium/PermutationInString.js
+++ Medium/PermutationInString.js
@@ -4,3 +4,16 @@ let i = 1;
 let piece = s2.slice(i, i + 3);
 
 console.log(piece);
+
+function permutation(s1, s2) {
+  s1 = s1.split("").sort().join("");
+
+  for (let i = 0; i < s2.length; i++) {
+    let piece = s2.slice(i, i + s1.length);
+    let perm = piece.split("").sort().join("");
+    if (perm === s1) {
+      return true;
+    }
+  }
+  return false;
+}
